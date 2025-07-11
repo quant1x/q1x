aff380708b1f8a16ad7f765507e3e2e3402df883
wangfeng <wangfengxy@sina.cn>
2025-07-11 15:08:46 +0800

    修正头文件宏

--- include/q1x/std/affinity.h
+++ include/q1x/std/affinity.h
@@ -1,8 +1,8 @@
 #pragma once
-#ifndef API_AFFINITY_H
-#define API_AFFINITY_H 1
+#ifndef QUANT1X_STD_AFFINITY_H
+#define QUANT1X_STD_AFFINITY_H 1
 
-#include "q1x/std/base.h"
+#include <q1x/std/base.h>
 
 // CPU亲和性
 namespace affinity {
@@ -10,4 +10,4 @@ namespace affinity {
     bool bind_current_thread_to_optimal_cpu(std::error_code &ec);
 }
 
-#endif // API_AFFINITY_H
+#endif // QUANT1X_STD_AFFINITY_H

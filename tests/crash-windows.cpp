17b5f0407914981df52646824d57535ced4169ff
wangfeng <wangfengxy@sina.cn>
2025-07-11 09:46:31 +0800

    测试Clang异常崩溃调用栈, 可以输出调用栈, 但是不精准, 只能定位到crash_function, 不能定位到*a=1这一行

--- tests/crash-windows.cpp
+++ tests/crash-windows.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#define BACKWARD_HAS_DWARF 0
+//#define BACKWARD_HAS_DWARF 0
 //#define BACKWARD_SYSTEM_UNKNOWN 0
 //#define BACKWARD_SYSTEM_LINUX 0
-#define BACKWARD_SYSTEM_WINDOWS 1  // 强制启用Windows模式
+//#define BACKWARD_SYSTEM_WINDOWS 1  // 强制启用Windows模式
 //#define BACKWARD_SYSTEM_DARWIN 0
+#define BACKWARD_HAS_UNWIND 1
 #include "backward.hpp"
 
 namespace {
@@ -18,5 +19,9 @@ void crash_function() {
 int main() {
     std::cout << "程序开始运行..." << std::endl;
     crash_function();
+    int a = 0;
+    a++;
+    ++a;
+    std::cout<< a << std::endl;
     return 0;
 }

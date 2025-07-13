c6479c179963949290451865276b35d221d70ba1
wangfeng <wangfengxy@sina.cn>
2025-07-13 10:27:54 +0800

    数据集各模块简要注释

--- src/q1x/datasets.cpp
+++ src/q1x/datasets.cpp
@@ -11,13 +11,17 @@ namespace datasets {
         // 基础数据
         // 除权除息
         cache::Register(std::make_unique<DataXdxr>());
-        // 日线
+        // 日线 - 未除权
         cache::Register(std::make_unique<DataKLineRaw>());
+        // 日线 - 除权
         cache::Register(std::make_unique<DataKLine>());
+        // 分时数据
         cache::Register(std::make_unique<DataMinute>());
 
         // 特征数据
+        // F10
         cache::Register(std::make_unique<F10Feature>());
+        // 通用历史数据
         cache::Register(std::make_unique<HistoryFeature>());
     }
 
